69273e63dbf36b7bc7346b4d4c00924ab976d151
pmelo-ca <pedromelocf@yahoo.com>
2023-08-23 14:52:08 -0300

    Creating loop

--- get_next_line.c
+++ get_next_line.c
@@ -6,12 +6,11 @@
 /*   By: pmelo-ca <pmelo-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/22 12:04:19 by pmelo-ca          #+#    #+#             */
-/*   Updated: 2023/08/23 14:29:56 by pmelo-ca         ###   ########.fr       */
+/*   Updated: 2023/08/23 14:50:08 by pmelo-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <stdio.h>
 
 char	*get_next_line(int fd)
 {
@@ -20,12 +19,15 @@ char	*get_next_line(int fd)
 	size_t	read_buffer;
 
 	read_buffer = read(fd, temp_string_buffer, BUFFER_SIZE);
-	if (!(ft_strchr(temp_string_buffer, '\n')))
+	while (!(ft_strchr(temp_string_buffer, '\n')))
+	{
 		strncat(actual_line, temp_string_buffer, read_buffer);
-	else
+		read_buffer = read(fd, temp_string_buffer, BUFFER_SIZE);
+	}
+	if (ft_strchr(temp_string_buffer, '\n'))
 		strncat(actual_line, temp_string_buffer, read_buffer);
-	printf("Conteudo da variável temporária : %s\n\n", temp_string_buffer);
-	printf("Conteudo da variável estática : %s\n\n", actual_line);
-	printf("Tamanho de bytes lidos  : %ld\n\n", read_buffer);
-	return (0);
+	// printf("Conteudo da variável temporária : %s\n\n", temp_string_buffer);
+	// printf("Conteudo da variável estática : %s\n\n", actual_line);
+	// printf("Tamanho de bytes lidos  : %ld\n\n", read_buffer);
+	return (actual_line);
 }
--- main.c
+++ main.c
@@ -1,14 +1,13 @@
 #include "get_next_line.h"
 #include <fcntl.h>
+#include <stdio.h>
 
 int main (void)
 {
 	int fd;
 
 	fd = open("test.txt", O_RDONLY);
-	get_next_line(fd);
-	get_next_line(fd);
-	get_next_line(fd);
+	printf("%s", get_next_line(fd));
 	close(fd);
 	return 0;
 }
