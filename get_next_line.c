f1c2333e724f4b9e0840765f8aee413378d9b3f5
pmelo-ca <pedromelocf@yahoo.com>
2023-08-23 11:04:50 -0300

    Creating actual_line, fixing variable names

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: pmelo-ca <pmelo-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/22 12:04:19 by pmelo-ca          #+#    #+#             */
-/*   Updated: 2023/08/22 23:45:53 by pmelo-ca         ###   ########.fr       */
+/*   Updated: 2023/08/23 11:03:22 by pmelo-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,13 @@
 
 char	*get_next_line(int fd)
 {
-	char		line[BUFFER_SIZE];
-	size_t 		test;
-	size_t	test1;
+	static char	*actual_line[3000];
+	char	temp_string_buffer[BUFFER_SIZE];
+	size_t	read_buffer;
 
-	test = BUFFER_SIZE;
-	test1 = read(fd, line, test);
-	line[test] = '\0';
-	printf("Conteudo do arquivo : %s\n", line);
-	printf("Conteudo do arquivo : %ld\n", test1);
+	read_buffer = read(fd, temp_string_buffer, BUFFER_SIZE);
+
+	printf("Conteudo do arquivo : %s\n", temp_string_buffer);
+	printf("Conteudo do arquivo : %ld\n\n", read_buffer);
 	return (0);
 }
