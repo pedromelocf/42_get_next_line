5dad72db1c08f81cc86af8ab0709b77364135f7a
pmelo-ca <pedromelocf@yahoo.com>
2023-08-30 17:28:50 -0300

    more tests

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: pmelo-ca <pmelo-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/22 12:04:19 by pmelo-ca          #+#    #+#             */
-/*   Updated: 2023/08/30 17:05:17 by pmelo-ca         ###   ########.fr       */
+/*   Updated: 2023/08/30 17:28:15 by pmelo-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,20 +22,23 @@ char	*get_next_line(int fd)
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	next_line_content = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
-	while (ft_strchr(buf, '\n') == NULL)
+	while (ft_strchr(actual_line_content, '\n') == NULL)
 	{
 		buf = (char *)malloc(sizeof(char) * BUFFER_SIZE);
 		read(fd, buf, BUFFER_SIZE);
-		strcat(actual_line_content, buf);
 		new_line = ft_strchr(buf, '\n');
 		if (new_line)
 		{
 			*new_line = '\0';
+			strcat(actual_line_content, buf);
 			memmove(next_line_content, new_line + 1, BUFFER_SIZE - (new_line - next_line_content) - 1);
 			break ;
 		}
 		else
+		{
+			strcat(actual_line_content, buf);
 			buf = NULL;
+		}
 	}
 	return (buf);
 }
