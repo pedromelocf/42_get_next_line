a8795c5bb8057dc060d434e0ded52865899b6dca
Pedro Melo <pedromelocf@yahoo.com>
2023-08-31 14:45:25 -0300

    Fixing free buf and verifying if read doesnt have any buffer read

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: pmelo-ca <pmelo-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/22 12:04:19 by pmelo-ca          #+#    #+#             */
-/*   Updated: 2023/08/30 22:20:23 by pmelo-ca         ###   ########.fr       */
+/*   Updated: 2023/08/31 14:42:46 by pmelo-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,21 +23,22 @@ char	*get_next_line(int fd)
 		return (NULL);
 	actual_line_content = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
 	if (next_line_content != NULL)
-		actual_line_content = next_line_content;
+		actual_line_content = strdup(next_line_content);
 	while (ft_strchr(actual_line_content, '\n') == NULL)
 	{
 		buf = (char *)malloc(sizeof(char) * BUFFER_SIZE);
-		read(fd, buf, BUFFER_SIZE);
+		if (read(fd, buf, BUFFER_SIZE) <= 0)
+			return (NULL);
 		new_line = ft_strchr(buf, '\n');
 		actual_line_content = ft_strjoin(actual_line_content, buf);
 		if (new_line)
 		{
 			*new_line = '\0';
 			next_line_content = ft_strdup(new_line + 1);
-			break ;
+			break;
 		}
 		else
-			buf = NULL;
+			free(buf);
 	}
 	return (actual_line_content);
 }
