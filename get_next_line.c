1eaaa3422769f2dba56c7f9ac5a708532a6edfba
Pedro Melo <pedromelocf@yahoo.com>
2023-09-03 00:41:57 -0300

    Recreating logic

--- get_next_line.c
+++ get_next_line.c
@@ -6,9 +6,46 @@
 /*   By: pmelo-ca <pmelo-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/22 12:04:19 by pmelo-ca          #+#    #+#             */
-/*   Updated: 2023/09/02 22:21:58 by pmelo-ca         ###   ########.fr       */
+/*   Updated: 2023/09/03 00:33:46 by pmelo-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+char	*get_next_line(int fd)
+{
+	static char	*next_line_content;
+	char	*actual_line_content;
+	char	*new_line;
+	char	*buffer_read;
+
+	actual_line_content = ft_strdup("");
+	if (BUFFER_SIZE <= 0 || fd < 0)
+		return(NULL);
+	if (next_line_content != NULL)
+	{
+		actual_line_content = ft_join_buf(actual_line_content, next_line_content);
+		new_line = ft_strchr(next_line_content, '\n');
+		next_line_content = ft_strdup(new_line + 1);
+	}
+	while (ft_strchr(actual_line_content, '\n') == NULL)
+	{
+		buffer_read = (char *)malloc(sizeof(char) * BUFFER_SIZE);
+		if (read(fd, buffer_read, BUFFER_SIZE) <= 0)
+		{
+			if(ft_strchr(actual_line_content, '\0') == NULL)
+				actual_line_content[strlen(actual_line_content)] = '\0';
+			return (NULL);
+		}
+		if (ft_strchr(buffer_read, '\n') != NULL)
+		{
+			actual_line_content = ft_new_line(actual_line_content, buffer_read);
+			new_line = ft_strchr(next_line_content, '\n');
+			next_line_content = ft_strdup(new_line + 1);
+			break;
+		}
+		if (ft_strchr(buffer_read, '\n') == NULL)
+			actual_line_content = ft_join_buf(actual_line_content, buffer_read);
+	}
+	return (actual_line_content);
+}
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,9 +6,99 @@
 /*   By: pmelo-ca <pmelo-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/22 12:04:17 by pmelo-ca          #+#    #+#             */
-/*   Updated: 2023/09/02 22:22:08 by pmelo-ca         ###   ########.fr       */
+/*   Updated: 2023/09/03 00:38:50 by pmelo-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+char	*ft_join_buf(char const *s1, char const *s2)
+{
+	char	*newstring;
+	size_t	counter;
+	size_t	s1_len;
+	size_t	s2_len;
+
+	s1_len = strlen(s1);
+	while(s2[s2_len] && s2[s2_len] != '\n')
+		s2_len++;
+	counter = 0;
+	newstring = (char *)malloc(sizeof(char) * s1_len + s2_len);
+	if (newstring == NULL)
+		return (NULL);
+	while (counter < s1_len + s2_len)
+	{
+		if (counter < s1_len)
+			newstring[counter] = s1[counter];
+		if (counter >= s1_len && counter < s1_len + s2_len)
+			newstring[counter] = s2[counter - s1_len];
+		counter++;
+	}
+	return (newstring);
+}
+char	*ft_new_line(char const *s1, char const *s2)
+{
+	char	*newstring;
+	size_t	counter;
+	size_t	s1_len;
+	size_t	s2_len;
+
+	s1_len = strlen(s1);
+	while(s2[s2_len] && s2[s2_len] != '\n')
+		s2_len++;
+	counter = 0;
+	newstring = (char *)malloc(sizeof(char) * s1_len + s2_len + 2);
+	if (newstring == NULL)
+		return (NULL);
+	while (counter < s1_len + s2_len)
+	{
+		if (counter < s1_len)
+			newstring[counter] = s1[counter];
+		if (counter >= s1_len && counter < s1_len + s2_len)
+			newstring[counter] = s2[counter - s1_len];
+		counter++;
+	}
+	newstring[counter] = '\n';
+	newstring[counter + 1] = '\0';
+	return (newstring);
+}
+
+char	*ft_strdup(const char *s)
+{
+	char	*newstring;
+	size_t	index1;
+	size_t	index2;
+
+	index1 = 0;
+	index2 = 0;
+	while (s[index1])
+		index1++;
+	newstring = (char *)malloc(sizeof(char) * index1 + 1);
+	if (!newstring)
+		return (NULL);
+	while (index2 < index1)
+	{
+		newstring[index2] = s[index2];
+		index2++;
+	}
+	newstring[index2] = '\0';
+	return (newstring);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	size_t	i;
+	size_t	s_len;
+
+	i = 0;
+	s_len = strlen(s);
+	while (i <= s_len)
+	{
+		if (s[i] == (unsigned char)c)
+			return ((char *)&s[i]);
+		i++;
+	}
+	if (c == 0)
+		return ((char *)&s[i]);
+	return (NULL);
+}
