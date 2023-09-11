03ba941a40878ceee96dac054cbba28f6f60e1cb
Pedro Melo <pedromelocf@yahoo.com>
2023-09-11 18:28:07 -0300

    Adding main corrections

--- bonus/main.c
+++ bonus/main.c
@@ -11,18 +11,18 @@ int	main(void)
 
 	fd1 = open("test.txt", O_RDONLY);
 	fd2 = open("test2.txt", O_RDONLY);
-	printf("%i \n", fd1);
-	printf("%i \n", fd2);
-	printf("Line 1 FD 1: %s", get_next_line(fd1));
-	printf("Line 1 FD 2: %s", get_next_line(fd2));
-	printf("Line 2 FD 1: %s", get_next_line(fd1));
-	printf("Line 2 FD 2: %s", get_next_line(fd2));
-	// counter = 1;
-	// while ((line = get_next_line(fd)) != NULL)
-	// {
-	// 	printf("Line %i : %s", counter++, line);
-	// 	free(line);
-	// }
+	counter = 1;
+	while ((line = get_next_line(fd1)) != NULL)
+	{
+		printf("Line %i : %s", counter++, line);
+		free(line);
+	}
+	counter = 1;
+	while ((line = get_next_line(fd2)) != NULL)
+	{
+		printf("Line %i : %s", counter++, line);
+		free(line);
+	}
 	close(fd1);
 	close(fd2);
 	return (0);
